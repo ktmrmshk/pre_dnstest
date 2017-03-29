42922aa41b1f5aa4335e668d8381c6145887a0b0
Masahiko Kitamura <ktmrmshk@gmail.com>
2017-03-29 20:46:34 +0900

    add, mod

--- dns_check.py
+++ dns_check.py
@@ -35,7 +35,9 @@ import dns.zone
 import dns.rdatatype
 import dns.name
 import dns.rdatatype
+import dns.rdataclass
 import sys
+
 
 
 import dns.ipv4
@@ -91,19 +93,87 @@ class DnsTester(object):
       authns_ip = self.authns
     else:
       authns_ip = self.resolv_ns(self.authns)
-    self.nameservers = [authns_ip]
+    self.resolver.nameservers = [authns_ip]
     print('authns_ip={}'.format(authns_ip)) 
 
-  def query_check(self, name, rdataset):
+  def get_query_answer(self, name, rdtype):
     '''
-    checks if name server has the entries same as one rdatasets has
+    return
+      - rdataset from query answer
     '''
     origin = dns.name.Name( self.zonename.split('.') )
 
     qname = name.derelativize(origin)
-    print('name={}, type={}'.format(qname.to_text(), dns.rdatatype.to_text(rdataset.rdtype)))
-    ans = self.resolver.query(qname.to_text(), dns.rdatatype.to_text(rdataset.rdtype))
+    print('name={}, type={} to NS={}'.format(qname.to_text(), dns.rdatatype.to_text(rdtype), self.resolver.nameservers) )
+    ans = self.resolver.query(qname, rdtype)
+    return ans.rrset
+
+  def query_check(self, name, rdataset):
+    '''
+    checks if name server has the entries same as one rdatasets has
+
+    args:
+      - name: Name object 
+      - rdataset: rdataset from zonefile tree
+
+    return
+      - list of tuple: (match, zonefile entry, query entry) 
+          like [(True, 'www.myexample.com. 1800  IN  A 10.20.30.40', 'www.myexample.com. 1800  IN  A 10.20.30.40'), ...] 
+    '''
+
+    origin = dns.name.Name( self.zonename.split('.') )
     
+    qr = self.get_query_answer(name, rdataset.rdtype)
+    zr = rdataset
+
+    ret = []
+    for rdata in zr:
+      flg = False
+      qtxt = '-'
+      if rdata in qr:
+        qtxt = self.rdata_to_text(origin, name, qr, rdata)
+        flg = True
+      ztxt = self.rdata_to_text(origin, name, zr, rdata)
+      ret.append( (flg, ztxt, qtxt) )
+    
+    r_diff = qr - zr
+    for rdata in r_diff:
+      flg = False
+      ztxt = '-'
+      qtxt = self.rdata_to_text(origin, name, qr, rdata)
+      ret.append( (flg, ztxt, qtxt) )
+
+    return ret
+    
+  def rdata_to_text(self, origin, name, rdataset, rdata):
+    'a text formatter like "www.myexample.com 1800 IN A 10.20.30.60"'
+    name_txt = name.derelativize(origin).to_text()
+    ttl = rdataset.ttl
+    cls_txt = dns.rdataclass.to_text(rdataset.rdclass)
+    rtype_txt = dns.rdatatype.to_text(rdataset.rdtype)
+    record_txt = rdata.to_text()
+    return '{} {} {} {} {}'.format( name_txt, ttl, cls_txt, rtype_txt, record_txt)
+
+    
+    
+  def get_rdataset_from_zone(self, name_txt, record_txt):
+    '''
+    for debuging purpose
+    return
+      - name obj
+      - rtype (int)
+      - rdataset
+    '''
+    name = dns.name.Name(name_txt.split('.'))
+    rtype = dns.rdatatype.from_text(record_txt)
+    rdata = self.zone.get_rdataset(name, rtype)
+    return (name, rtype, rdata)
+
+    
+
+       
+
+      
 
 
 
--- test_dns_check.py
+++ test_dns_check.py
@@ -2,17 +2,35 @@ import unittest
 
 from dns_check import is_ip, DnsTester
 import dns.name
-
+import dns.rdatatype
+import dns.rrset
+import copy
 
 class TestDnsCheck(unittest.TestCase):
   def setUp(self):
     self.dt = DnsTester('zone_revised.txt', 'a12-65.akam.net.')
-    
 
-  #def test_query_check(self):
-  #  name = dns.name.Name('www')
-  #  dt.query_check(name, None)
-    
+  def test_get_query_answer(self):
+    name, rdtype, rdataset = self.dt.get_rdataset_from_zone('www', 'A')
+    ret = self.dt.get_query_answer(name, rdtype)
+    self.assertEqual( type(ret), dns.rrset.RRset )
+
+  def test_query_check(self):
+    name, rdtype, rdataset = self.dt.get_rdataset_from_zone('www', 'A')
+    ret = self.dt.query_check(name, rdataset)
+    print(ret)
+
+  def test_get_rdata_from_zone1(self):
+    ret = self.dt.get_rdataset_from_zone('www', 'A')
+    self.assertEqual( ret[0].to_text(), 'www')
+    self.assertEqual( dns.rdatatype.to_text( ret[1] ), 'A'  )
+    self.assertIsNotNone(ret[2])
+
+  def test_get_rdata_from_zone2(self):
+    ret = self.dt.get_rdataset_from_zone('www123', 'NS')
+    self.assertEqual( ret[0].to_text(), 'www123')
+    self.assertEqual( dns.rdatatype.to_text( ret[1] ), 'NS'  )
+    self.assertIsNone(ret[2])
 
   
   def test_is_ip1(self):
